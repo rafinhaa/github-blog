7ec8e0a93299c2a36b3bd46ff371329e626b2069
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-09 10:57:22 -0300

    feat: Implementa componente DefaultLayout

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import { DefaultLayout } from "../layouts/defaultLayout";
 
 import { Home } from "../pages/Home";
 import { Post } from "../pages/Post";
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/post" element={<Post />} />
+    <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/post" element={<Post />} />
+    </Route>
   </Routes>
 );
