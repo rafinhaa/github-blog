9a518f0cc81ab3ebc3a1b72577f188f6c75bdcd7
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-09 10:55:20 -0300

    feat: Cria componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import logo from "../../assets/logo.svg";
+import { HeaderContainer } from "./styles";
+
+export const Header = () => {
+  return <HeaderContainer />;
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import backgroundHeader from "../../assets/backgroundHeader.svg";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 18.5rem;
+  background: url(${backgroundHeader}) no-repeat center;
+  background-size: cover;
+`;
