fa219279f97e4f2fe630e145ff23862821d9f3f4
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-10 11:36:52 -0300

    feat: Implementa margin para o defaultLayout

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { LayoutContainer } from "./styles";
 
 export const DefaultLayout = () => {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
-      <Outlet />
-    </div>
+      <main>
+        <Outlet />
+      </main>
+    </LayoutContainer>
   );
 };
