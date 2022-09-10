eaa1c55adf102bf204844bba96aab49e1b5fd295
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-10 11:33:47 -0300

    refactor: Typography Link
    Implementa ícones.

--- src/components/Typography/Link/styles.ts
+++ src/components/Typography/Link/styles.ts
@@ -1,11 +1,22 @@
 import styled from "styled-components";
-import { TLinkProps } from "./types";
 
-type TLinkContainerProps = Omit<TLinkProps, "children">;
-
-export const LinkContainer = styled.p<TLinkContainerProps>`
+export const LinkContainer = styled.a`
   font-size: 0.75rem;
   color: ${({ theme }) => theme["blue"]};
-  weight: 700;
+  font-weight: 700;
   line-height: "160%";
+  text-transform: uppercase;
+  border-bottom: 1px solid transparent;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  svg {
+    height: 0.75rem;
+    width: 0.75rem;
+  }
+
+  &:hover {
+    border-color: ${({ theme }) => theme["blue"]};
+  }
 `;
