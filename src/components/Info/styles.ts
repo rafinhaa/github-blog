2b334abf50b0c5562839e0365846bfc1e5c00aec
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-10 11:38:17 -0300

    feat: Cria componente Info

---
+++ src/components/Info/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import backgroundHeader from "../../assets/backgroundHeader.svg";
+
+export const InfoLabelContainer = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme["base-label"]};
+  svg {
+    height: 1.125rem;
+    width: 1.125rem;
+  }
+`;
