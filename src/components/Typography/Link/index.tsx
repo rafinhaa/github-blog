6ff7225f54539456a55498bf6ca976176ab8fbb8
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-10 11:33:47 -0300

    refactor: Typography Link
    Implementa ícones.

--- src/components/Typography/Link/index.tsx
+++ src/components/Typography/Link/index.tsx
@@ -1,6 +1,43 @@
+import {
+  faChevronLeft,
+  faUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ComponentProps, ReactNode } from "react";
 import { LinkContainer } from "./styles";
-import { TLinkProps } from "./types";
 
-export const Link = ({ children }: TLinkProps) => {
-  return <LinkContainer>{children}</LinkContainer>;
+type TLinkProps = ComponentProps<typeof LinkContainer> & {
+  children: ReactNode;
+  type: "back" | "external";
+};
+
+export const Link = ({ children, type, ...rest }: TLinkProps) => {
+  const IconRight = () => {
+    return (
+      <>
+        {children} <FontAwesomeIcon icon={faUpRightFromSquare} />
+      </>
+    );
+  };
+
+  const IconLeft = () => {
+    return (
+      <>
+        <FontAwesomeIcon icon={faChevronLeft} /> {children}
+      </>
+    );
+  };
+
+  const childrenMapping = {
+    back: IconLeft,
+    external: IconRight,
+  };
+
+  const Children = childrenMapping[type];
+
+  return (
+    <LinkContainer {...rest}>
+      <Children />
+    </LinkContainer>
+  );
 };
