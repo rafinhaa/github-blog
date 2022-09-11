078171b47691568e9e8e59383579d54bd4f904a7
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-11 11:31:16 -0300

    feat: Implementa componente Post na seção PostList

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,18 @@
+import { Post } from "./components/Post";
 import { Profile } from "./sections/Profile";
 import { Search } from "./sections/Search";
+import { PostListContainer } from "./styles";
 
 export const Home = () => {
   return (
     <section>
       <Profile />
       <Search />
+      <PostListContainer>
+        <Post />
+        <Post />
+        <Post />
+      </PostListContainer>
     </section>
   );
 };
