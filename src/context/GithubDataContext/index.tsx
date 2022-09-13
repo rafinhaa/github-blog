094722a5aba0df4002a48ebc7165e7b75fdeb896
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-13 09:55:19 -0300

    feat: Implementa função de listagem de issues

--- src/context/GithubDataContext/index.tsx
+++ src/context/GithubDataContext/index.tsx
@@ -1,13 +1,21 @@
 import { createContext, FC, useCallback, useEffect, useState } from "react";
 import { api } from "../../services/api";
-import { IProfile, IProfileContextType, ProfileProviderProps } from "./types";
+import {
+  IIssues,
+  IProfile,
+  IProfileContextType,
+  ProfileProviderProps,
+} from "./types";
 
 const GITHUB_BLOG_PROFILE_STORAGE_KEY = "@githubBlog:profile";
 const gitHubUser = import.meta.env.VITE_GITHUB_USERNAME;
+const gitHubRepo = import.meta.env.VITE_GITHUB_REPONAME;
 
 export const GithubDataContext = createContext<IProfileContextType>({
   profile: {} as IProfile,
+  issues: {} as IIssues,
   getProfileInfo: async () => undefined,
+  getIssues: async (query: string) => undefined,
 });
 
 export const GithubDataProvider: FC<ProfileProviderProps> = ({ children }) => {
@@ -18,6 +26,7 @@ export const GithubDataProvider: FC<ProfileProviderProps> = ({ children }) => {
     if (storedProfileData) return JSON.parse(storedProfileData) as IProfile;
     return {} as IProfile;
   });
+  const [issues, setIssues] = useState<IIssues>({} as IIssues);
 
   const getProfileInfo = useCallback(async () => {
     const { data } = await api.get(`/users/${gitHubUser}`);
@@ -26,7 +35,17 @@ export const GithubDataProvider: FC<ProfileProviderProps> = ({ children }) => {
     localStorage.setItem(GITHUB_BLOG_PROFILE_STORAGE_KEY, JSON.stringify(data));
   }, []);
 
+  const getIssues = useCallback(async (query: string = "") => {
+    const { data } = await api.get(
+      `/search/issues?q=${query}%20repo:${gitHubUser}/${gitHubRepo}`
+    );
+
+    setIssues(data);
+    console.log(data);
+  }, []);
+
   useEffect(() => {
+    getIssues();
     const storedProfileData = localStorage.getItem(
       GITHUB_BLOG_PROFILE_STORAGE_KEY
     );
@@ -42,7 +61,9 @@ export const GithubDataProvider: FC<ProfileProviderProps> = ({ children }) => {
     <GithubDataContext.Provider
       value={{
         profile,
+        issues,
         getProfileInfo,
+        getIssues,
       }}
     >
       {children}
