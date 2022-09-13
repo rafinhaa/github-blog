ea11041a8d8ff652c8dd2786163733ea8ff176a3
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-09-13 09:55:19 -0300

    feat: Implementa função de listagem de issues

--- src/context/GithubDataContext/types.ts
+++ src/context/GithubDataContext/types.ts
@@ -9,10 +9,27 @@ export interface IProfile {
   followers: number;
   html_url: string;
 }
+export interface IIssues {
+  total_count: number;
+  items: {
+    title: string;
+    body: string;
+    id: number;
+    number: number;
+    html_url: string;
+    comments: number;
+    created_at: string;
+    user: {
+      login: string;
+    };
+  }[];
+}
 
 export interface IProfileContextType {
   profile: IProfile;
+  issues: IIssues;
   getProfileInfo: () => Promise<void>;
+  getIssues: (query: string) => Promise<void>;
 }
 
 export interface ProfileProviderProps {
